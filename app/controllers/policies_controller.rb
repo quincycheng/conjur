b4865e6e621eccaba5c00f8013ce9da4debd5e34
Micah Lee <micah.lee@cyberark.com>
2019-07-23 17:26:17 -0400

    !dev Add rudimentary variable provisioning from policy load context

--- app/controllers/policies_controller.rb
+++ app/controllers/policies_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'multipart_parser/reader'
+
 class PoliciesController < RestController
   include FindResource
   include AuthorizeResource
@@ -17,7 +19,7 @@ class PoliciesController < RestController
     authorize :update
 
     policy = save_submitted_policy(delete_permitted: true)
-    replace_policy = Loader::ReplacePolicy.from_policy(policy)
+    replace_policy = Loader::ReplacePolicy.from_policy(policy, context: policy_context)
     created_roles = perform(replace_policy)
 
     render json: {
@@ -30,7 +32,7 @@ class PoliciesController < RestController
     authorize :update
 
     policy = save_submitted_policy(delete_permitted: true)
-    modify_policy = Loader::ModifyPolicy.from_policy(policy)
+    modify_policy = Loader::ModifyPolicy.from_policy(policy, context: policy_context)
     created_roles = perform(modify_policy)
 
     render json: {
@@ -43,7 +45,7 @@ class PoliciesController < RestController
     authorize :create
 
     policy = save_submitted_policy(delete_permitted: false)
-    create_policy = Loader::CreatePolicy.from_policy(policy)
+    create_policy = Loader::CreatePolicy.from_policy(policy, context: policy_context)
     created_roles = perform(create_policy)
 
     render json: {
@@ -59,6 +61,52 @@ class PoliciesController < RestController
     policy_action.call
     new_actor_roles = actor_roles(policy_action.new_roles)
     create_roles(new_actor_roles)
+
+  end
+
+  def policy_text
+    case request.content_type
+    when 'multipart/form-data'
+      multipart_data[:policy]
+    else
+      request.raw_post
+    end
+  end
+
+  def policy_context
+    multipart_data.reject { |k,v| k == :policy }
+  end
+
+  def multipart_data
+    @multipart_data ||= parse_multipart_data
+  end
+
+  def parse_multipart_data
+    boundary = MultipartParser::Reader::extract_boundary_value(request.headers['CONTENT_TYPE'])
+    reader = MultipartParser::Reader.new(boundary)
+
+    parts={}
+
+    reader.on_part do |part|
+      pn = part.name.to_sym
+      part.on_data do |partial_data|
+        if parts[pn].nil?
+          parts[pn] = partial_data
+        else
+          parts[pn] = [parts[pn]] unless parts[pn].kind_of?(Array)
+          parts[pn] << partial_data
+        end
+      end
+    end
+
+    reader.on_error do |err|
+      $stderr.puts("Error: #{err}")
+    end
+
+    reader.write request.raw_post.encode(crlf_newline: true)
+    reader.ended? or raise Exception, 'truncated multipart message'
+
+    parts
   end
 
   def find_or_create_root_policy
@@ -87,7 +135,7 @@ class PoliciesController < RestController
     policy_version = PolicyVersion.new(
       role: current_user,
       policy: resource,
-      policy_text: request.raw_post,
+      policy_text: policy_text,
       client_ip: request.ip
     )
     policy_version.delete_permitted = delete_permitted

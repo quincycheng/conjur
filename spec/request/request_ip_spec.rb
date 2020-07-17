0c17c558c9745d8f2e3f061bd0aa25f870a12650
Micah Lee <micah.lee@cyberark.com>
2020-07-17 12:27:25 -0400

    fixup! refactor tests

--- spec/request/request_ip_spec.rb
+++ spec/request/request_ip_spec.rb
@@ -2,14 +2,7 @@
 
 require 'spec_helper'
 
-RSpec.shared_examples "expected request IP address" do 
-  it "uses the expected request IP address" do
-    get '/request_ip', env: request_env, headers: headers
-    expect(body_hash['ip']).to eq(expected_request_ip)
-  end
-end
-
-RSpec.describe "request IP address determination", :type => :request do
+RSpec.describe "request IP address determination", type: :request do
   # We inject this RequestIpController to provide our tests with information
   # about the IP address available to Rails controllers
   class RequestIpController < ApplicationController
@@ -18,17 +11,39 @@ RSpec.describe "request IP address determination", :type => :request do
     end
   end
 
+  # :reek:UtilityFunction
+  def token_auth_header
+    # Configure Slosilo to produce valid access tokens
+    slosilo = Slosilo["authn:rspec"] ||= Slosilo::Key.new
+    bearer_token = slosilo.signed_token('admin')
+    "Token token=\"#{Base64.strict_encode64 bearer_token.to_json}\""
+  end
+
+  def request_env(remote_addr)
+    {
+      # We can't modify the access token middleware to add an exception to our
+      # test route, so we need to create an access token for our request to use.
+      'HTTP_AUTHORIZATION' => token_auth_header,
+      'REMOTE_ADDR' => remote_addr
+    }
+  end
+
+  def request_ip(remote_addr:, x_forwarded_for: nil, trusted_proxies: nil)
+    ENV['TRUSTED_PROXIES'] = trusted_proxies if trusted_proxies
+  
+    headers = {}
+    headers['X-Forwarded-For'] = x_forwarded_for if x_forwarded_for
+  
+    get '/request_ip', env: request_env(remote_addr), headers: headers
+  
+    JSON.parse(response.body)['ip']
+  end
+
   before do
     # Add the test endpoint to the routes
     Rails.application.routes.draw do
       get '/request_ip' => 'request_ip#echo'
     end
-
-    # Configure Slosilo to produce valid access tokens
-    Slosilo["authn:rspec"] ||= Slosilo::Key.new
-
-    # Set the TRUSTED_PROXIES environment variable for the test
-    ENV['TRUSTED_PROXIES'] = trusted_proxies if trusted_proxies
   end
 
   after do
@@ -37,143 +52,127 @@ RSpec.describe "request IP address determination", :type => :request do
   end
 
   # --------------------------------------------------------------------
-  # Test Setup
-  # --------------------------------------------------------------------
-
-  let(:body_hash) { JSON.parse(response.body) }
-
-  # We can't modify the access token middleware to add an exception to our
-  # test route, so we need to create an access token for our request to use.
-  let(:bearer_token) { Slosilo["authn:rspec"].signed_token('admin') }
-  let(:token_auth_header) do
-    "Token token=\"#{Base64.strict_encode64 bearer_token.to_json}\""
-  end
-
-  let(:request_env) do
-    { 'HTTP_AUTHORIZATION' => token_auth_header, 'REMOTE_ADDR' => remote_ip }
-  end
-
-  let(:headers) do
-    {}.tap do |h|
-      h['X-Forwarded-For'] = x_forwarded_for if x_forwarded_for
-    end
-  end
-
-  # The value to user for the TRUSTED_PROXIES environment variable
-  let(:trusted_proxies) { nil }
-
-  # The value to use for the X-Forwarded-For HTTP header
-  let(:x_forwarded_for) { nil }
-
-  # --------------------------------------------------------------------
   # Test Scenarios
   # --------------------------------------------------------------------
 
   # Without any other configuration in play, we expect to get the remote
   # TCP connection IP address as the request IP address.
-  let(:remote_ip) { '44.0.0.1' }
-  let(:expected_request_ip) { '44.0.0.1' }
-  include_examples "expected request IP address"
-
-  # When X-Forwarded-For is set from an untrusted remote IP, we ignore it
-  context 'when X-Forwarded-For is set' do
-    let(:x_forwarded_for) { '3.3.3.3' }
-    let(:expected_request_ip) { '44.0.0.1' }
-    include_examples "expected request IP address"
-
-    # Loopback is a default trusted IP, so we accept the forwarded address
-    context 'when remote IP is loopback' do
-      let(:remote_ip) { '127.0.0.1' }
-      let(:expected_request_ip) { '3.3.3.3' }
-      include_examples "expected request IP address"
-    end
+  it 'returns the remote_addr with no additional config' do
+    expect(request_ip(remote_addr: '44.0.0.1')).to eq('44.0.0.1')
   end
 
-  context 'when TRUSTED_PROXIES is set' do
-    # If TRUSTED proxies are set, and are remote IP address is not trusted
-    # we expect to get the remote IP
-    let(:trusted_proxies) { '4.4.4.4,5.5.5.5' }
-    let(:expected_request_ip) { '44.0.0.1' }
-    include_examples "expected request IP address"
+  # When X-Forwarded-For is set from an untrusted remote IP, we ignore it
+  it 'ignores the XFF header when the remote addr is untrusted' do
+    expect(
+      request_ip(
+        remote_addr: '44.0.0.1',
+        x_forwarded_for: '3.3.3.3'
+      )
+    ).to eq('44.0.0.1')
+  end
 
-    context 'when remote IP is loopback' do
-      let(:remote_ip) { '127.0.0.1' }
-      let(:expected_request_ip) { '127.0.0.1' }
-      include_examples "expected request IP address"
+  # By default "non-routable" IP addresses are trusted (according to this
+  # regular expression: https://github.com/rack/rack/blob/master/lib/rack/request.rb#L19)
+  #
+  # `127.0.0.1` is important as the address of the nginx proxy when used in DAP
+  it 'trusts the loopback address by default to provide XFF' do
+    expect(
+      request_ip(
+        remote_addr: '127.0.0.1',
+        x_forwarded_for: '3.3.3.3'
+      )
+    ).to eq('3.3.3.3')
+  end
 
-      # Loopback is always trusted, even if not explicity provided in
-      # TRUSTED_PROXIES, so we accept the forwarded IP address.
-      context 'when X-Forwarded-For is set' do
-        let(:x_forwarded_for) { '3.3.3.3' }
-        let(:expected_request_ip) { '3.3.3.3' }
-        include_examples "expected request IP address"
-      end
-    end
+  # If TRUSTED proxies are set, and are remote IP address is not trusted
+  # we expect to get the remote IP
+  it "doesn't trust the remote_addr if not included in TRUSTED_PROXIES" do
+    expect(
+      request_ip(
+        remote_addr: '44.0.0.1',
+        x_forwarded_for: '3.3.3.3',
+        trusted_proxies: '4.4.4.4'
+      )
+    ).to eq('44.0.0.1')
+  end
 
-    context 'when using CIDR notation for trusted IP ranges' do
-      let(:trusted_proxies) { '5.5.5.0/24' }
-      let(:remote_ip) { '5.5.5.1' }
-      let(:x_forwarded_for) { '3.3.3.3,5.5.5.2,5.5.5.3' }
-      let(:expected_request_ip) { '3.3.3.3' }
-      include_examples "expected request IP address"
+  it "trusts 127.0.0.1 for XFF even when not included explicitly with TRUSTED_PROXIES" do
+    expect(
+      request_ip(
+        remote_addr: '127.0.0.1',
+        x_forwarded_for: '3.3.3.3',
+        trusted_proxies: '4.4.4.4'
+      )
+    ).to eq('3.3.3.3')
+  end
+  
+  it "trusts IP ranges for XFF using CIDR notation in TRUSTED_PROXIES" do
+    expect(
+      request_ip(
+        remote_addr: '5.5.5.1',
+        x_forwarded_for: '3.3.3.3',
+        trusted_proxies: '5.5.5.0/24'
+      )
+    ).to eq('3.3.3.3')
+  end
 
-      context 'when multiple IP ranges are trusted' do
-        let(:trusted_proxies) { '4.4.4.0/24,5.5.5.0/24' }
-        let(:remote_ip) { '5.5.5.1' }
-        let(:x_forwarded_for) { '3.3.3.3,4.4.4.1,5.5.5.3' }
-        let(:expected_request_ip) { '3.3.3.3' }
-        include_examples "expected request IP address"
-      end
-    end
+  it "returns the expected IP when multiple XFF values are included" do
+    expect(
+      request_ip(
+        remote_addr: '5.5.5.1',
+        x_forwarded_for: '3.3.3.3,5.5.5.2,5.5.5.3',
+        trusted_proxies: '5.5.5.0/24'
+      )
+    ).to eq('3.3.3.3')
+  end
 
-    # The following spec exercise how variations on the X-Forwarded-For header
-    # trusted proxies translate into the final IP address for the Rails request.
-    context 'when remote IP is trusted' do
-      let(:remote_ip) { '4.4.4.4' }
-      let(:expected_request_ip) { '4.4.4.4' }
-      include_examples "expected request IP address"
+  it "returns the expected IP when multiple ranges are included in TRUSTED_PROXIES" do
+    expect(
+      request_ip(
+        remote_addr: '4.4.4.4',
+        x_forwarded_for: '3.3.3.3,5.5.5.2,5.5.5.3',
+        trusted_proxies: '5.5.5.0/24,4.4.4.0/24'
+      )
+    ).to eq('3.3.3.3')
+  end
 
-      context 'when X-Forwarded-For is set' do
-        let(:x_forwarded_for) { '3.3.3.3' }
-        let(:expected_request_ip) { '3.3.3.3' }
-        include_examples "expected request IP address"
-      end
+  it "returns the right-most untrusted IP when XFF contains multiple untrusted IPs" do
+    expect(
+      request_ip(
+        remote_addr: '4.4.4.4',
+        x_forwarded_for: '3.3.3.3,6.6.6.6,7.7.7.7',
+        trusted_proxies: '4.4.4.0/24'
+      )
+    ).to eq('7.7.7.7')
+  end
 
-      context 'when X-Forwarded-For contains multiple untrusted IPs' do
-        let(:x_forwarded_for) { '3.3.3.3,6.6.6.6,7.7.7.7' }
-        # it returns the right-most
-        let(:expected_request_ip) { '7.7.7.7' }
-        include_examples "expected request IP address"
-      end
+  it "returns the right-most untrusted IP when XFF contains some trusted IPs" do
+    expect(
+      request_ip(
+        remote_addr: '4.4.4.4',
+        x_forwarded_for: '3.3.3.3,6.6.6.6,5.5.5.5',
+        trusted_proxies: '4.4.4.0/24,5.5.5.0/24'
+      )
+    ).to eq('6.6.6.6')
+  end
 
-      context 'when X-Forwarded-For contains some trusted IPs' do
-        let(:x_forwarded_for) { '3.3.3.3, 5.5.5.5' }
-        # it returns the right-most untrusted value
-        let(:expected_request_ip) { '3.3.3.3' }
-        include_examples "expected request IP address"
-      end
+  it "returns the right-most untrusted IP when XFF contains a trusted IP in the middle" do
+    expect(
+      request_ip(
+        remote_addr: '4.4.4.4',
+        x_forwarded_for: '3.3.3.3,5.5.5.5,6.6.6.6,7.7.7.7',
+        trusted_proxies: '4.4.4.0/24,5.5.5.0/24'
+      )
+    ).to eq('7.7.7.7')
+  end
 
-      context 'when X-Forwarded-For contains multiple untrusted IPs' do
-        let(:x_forwarded_for) { '3.3.3.3,6.6.6.6,7.7.7.7,5.5.5.5' }
-        # it returns the right-most untrusted value
-        let(:expected_request_ip) { '7.7.7.7' }
-        include_examples "expected request IP address"
-      end
-
-      context 'when X-Forwarded-For contains some trusted IP in the middle' do
-        let(:x_forwarded_for) { '3.3.3.3,5.5.5.5,6.6.6.6,7.7.7.7' }
-        # it returns the right-most untrusted value
-        let(:expected_request_ip) { '7.7.7.7' }
-        include_examples "expected request IP address"
-      end
-
-      context 'when X-Forwarded-For contains all trusted IPs' do
-        let(:trusted_proxies) { '4.4.4.4,5.5.5.5,6.6.6.6' }
-        let(:x_forwarded_for) { '5.5.5.5,6.6.6.6' }
-        # it returns the left-most trusted value
-        let(:expected_request_ip) { '5.5.5.5' }
-        include_examples "expected request IP address"
-      end
-    end
+  it "returns the left-most trusted IP when XFF contains all trusted IPs" do
+    expect(
+      request_ip(
+        remote_addr: '4.4.4.4',
+        x_forwarded_for: '5.5.5.5,6.6.6.6',
+        trusted_proxies: '4.4.4.4,5.5.5.5,6.6.6.6'
+      )
+    ).to eq('5.5.5.5')
   end
 end

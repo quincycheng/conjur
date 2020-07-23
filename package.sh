0a5f345cda650d20f9c2c69358434ac95fd8e6d1
Alon Carmel <alon.carmel@cyberark.com>
2020-07-23 11:51:01 +0300

    update conjur base image

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
-export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11.5'
-
+export DEBIFY_IMAGE='registry.tld/conjurinc/debify:1.11.5.1-0251923'
+docker pull registry.tld/cyberark/phusion-ruby-fips:0.11-d243f6c
 docker run --rm $DEBIFY_IMAGE config script > docker-debify
 chmod +x docker-debify
 

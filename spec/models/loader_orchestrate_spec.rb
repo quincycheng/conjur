0b363f06c1792da440f58780e47be6b261e351f3
Micah Lee <micah.lee@cyberark.com>
2020-09-04 13:49:35 -0400

    fixup!

--- spec/models/loader_orchestrate_spec.rb
+++ spec/models/loader_orchestrate_spec.rb
@@ -74,7 +74,7 @@ describe Loader::Orchestrate do
         verify_data 'updated/simple.txt'
       end
       it "creates a policy factory" do
-        load_policy_update 'policy_factory.yml'
+        replace_policy_with 'policy_factory.yml'
         verify_data 'updated/policy_factory.txt'
       end
       it "creates a host factory" do
